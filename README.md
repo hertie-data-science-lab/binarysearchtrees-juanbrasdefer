25c165eea2f809d34575ea0075db131064056e73
juanbrasdefer <juan.brasdefer@sciencespo.fr>
2023-04-24 19:03:03 +0200

    Working?

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/L0P4fZaj)
+
+# Documentation:
+This Modular Code makes use of a classical "Tree" template and a "Binary Tree" template to add the feature of BinarySearch, which turns the original Binary Tree into a sorted version of itself, where:
+1) The value of the left child of any node is lesser than the value of the node itself
+2) The value of the right child of any node is greater than the value of the node itself
+
+Binary Search Trees allow for quicker searching of node value than the standard Binary Trees, which means, by extension, that insertion and deletion are quicker as well.
+
+In order to turn a BinaryTree into a BinarySearchTree, this code uses the following logic:
+1) Perform an in-order (TraverseLeft, VisitNode, TraverseRight) traversal of the binary tree and store the values of the visited nodes in a list
+2) Sort the list
+3) Take the sorted list and use it to build a new (sorted) binary search tree :))
+
+
+
+
+
+
 # Assignment:
 
 Write a program that converts a binary tree into a binary search tree. I will not tell you the steps for how to do this, you will need to figure out the algorithm. However, here are some hints:
--- binarysearchtree.py
+++ binarysearchtree.py
@@ -6,6 +6,7 @@ Created on Tue Mar 28 15:16:50 2023
 """
 
 #this is a copy-paste of the old binarytree.py with some added flare :))
+#more explanation can be found in the README file
 
 
 from treetemplate import Tree
@@ -48,3 +49,43 @@ class BinaryTree(Tree):
         
         if self.right(p) is not None:
             yield self.right(p)
+
+
+
+
+
+
+
+
+
+    #Below is the new code for binary search tree
+    def inorder_traversal(self, p):
+        """Perform an in-order traversal of the subtree rooted at Position p."""
+        if p is not None:
+            for value in self.inorder_traversal(self.left(p)):
+                yield value
+            yield p.element()
+            for value in self.inorder_traversal(self.right(p)):
+                yield value
+                
+
+
+    def build_bst(self, values):
+        """Build a binary search tree using the given list of values."""
+        values = sorted(values)
+        mid = len(values) // 2
+        root = self._add_root(values[mid])
+        self._build_bst(root, values[:mid])
+        self._build_bst(root, values[mid + 1:])
+        return root
+    
+
+
+    def _build_bst(self, parent, values):
+        """Recursively build a binary search tree using the given list of values."""
+        if not values:
+            return
+        mid = len(values) // 2
+        child = self._add_left(parent, values[mid])
+        self._build_bst(child, values[:mid])
+        self._build_bst(child, values[mid + 1:])
